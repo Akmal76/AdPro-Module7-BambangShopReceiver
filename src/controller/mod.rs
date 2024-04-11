5d6deaa497fb978f2de23ce8d26192288751a5ab
Akmal76 <akmalramadhan76@gmail.com>
2024-04-11 20:38:03 +0700

    Implement the receive function in Notification controller.

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -5,6 +5,6 @@ use rocket::fairing::AdHoc;
 pub fn route_stage() -> AdHoc {
     return AdHoc::on_ignite("Initializing controller routes...", |rocket| async {
         rocket
-            .mount("/", routes![notification::subscribe, notification::unsubscribe])
+            .mount("/", routes![notification::subscribe, notification::unsubscribe, notification::receive])
     });
 }
--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -20,3 +20,11 @@ pub fn unsubscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
         Err(e) => Err(e)
     };
 }
+
+#[post("/receive", data="<notification>")]
+pub fn receive(notification: Json<Notification>)-> Result<Json<Notification>> {
+    return match NotificationService::receive_notification(notification.into_inner()){
+        Ok(f) => Ok(Json::from(f)),
+        Err(e) => Err(e)
+    };
+}
