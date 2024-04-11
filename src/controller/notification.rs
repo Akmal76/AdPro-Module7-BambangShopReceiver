9991a492fec1616f8e5f5d40cea7244cd258b9e9
Akmal76 <akmalramadhan76@gmail.com>
2024-04-11 20:39:44 +0700

    Implement list function in Notification controller.

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -28,3 +28,11 @@ pub fn receive(notification: Json<Notification>)-> Result<Json<Notification>> {
         Err(e) => Err(e)
     };
 }
+
+#[get("/")]
+pub fn list() -> Result<Json<Vec<String>>> {
+    return match NotificationService::list_message() {
+        Ok(f) => Ok(Json::from(f)),
+        Err(e) => Err(e)
+    };
+}
