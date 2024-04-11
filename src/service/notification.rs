55f39c01aa6d0c8f87a01ec8980ccbe904f164b3
Akmal76 <akmalramadhan76@gmail.com>
2024-04-11 20:33:59 +0700

    Commit your changes to Git with the message: “Implement unsubscribe function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -54,4 +54,40 @@ impl NotificationService{
             ))
         }
     }
+
+    pub fn unsubscribe(product_type: &str) -> Result<SubscriberRequest> {
+        let product_type_clone = String::from(product_type);
+        return thread::spawn(move|| Self::unsubscribe_request(product_type_clone))
+            .join().unwrap();
+    }
+
+    #[tokio::main]
+    async fn unsubscribe_request(product_type: String)-> Result<SubscriberRequest> {
+        let product_type_upper = product_type.to_uppercase();
+        let product_type_str: &str = &product_type_upper.as_str();
+        let notification_receiver_url: String = format!("{}/receive",
+            APP_CONFIG.get_instance_root_url());
+
+        let request_url: String = format!("{}/notification/unsubscribe/{}?url={}",
+            APP_CONFIG.get_publisher_root_url(), product_type_str, notification_receiver_url);
+        let request = REQWEST_CLIENT
+            .post(request_url.clone())
+            .header("Content-Type","application/json")
+            .header("Accept", "application/json")
+            .send().await;
+        log::warn_!("Sent unsubscribe request to: {}", request_url);
+
+        return match request{
+            Ok(f) => match f.json::<SubscriberRequest>().await {
+                Ok(x) => Ok(x),
+                Err(_y) => Err(compose_error_response(
+                    Status::NotFound,
+                    String::from("Already unsubscribed to the topic.")
+                ))
+            }
+            Err(e) =>Err(compose_error_response(
+                Status::NotFound,
+                e.to_string()))
+        }
+    }
 }
